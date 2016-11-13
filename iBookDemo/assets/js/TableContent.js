3c39a310317d4a20bdd75e0e6f69d0262e750a22
ChinhNC <chinh0510@gmail.com>
2016-11-13 22:30:48 +0700

    First commit on Github

---
+++ iBookDemo/assets/js/TableContent.js
@@ -0,0 +1,345 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 1020,
+	height: 760,
+	fps: 24,
+	color: "#FFFFFF",
+	manifest: [
+		{src:"../images/xau.jpg", id:"xau"},
+		{src:"../media/U1.mp3", id:"U1"}
+	]
+};
+
+
+
+// symbols:
+
+
+
+(lib.xau = function() {
+	this.initialize(img.xau);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1020,760);
+
+
+(lib.Symbol6 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("rgba(78,78,78,0.047)").s().p("AmECpQhkAAAAhkIAAiJQAAhkBkAAIMJAAQBkAAAABkIAACJQAABkhkAAg");
+
+	this.addChild(this.shape);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(-48.9,-16.9,98,33.9);
+
+
+(lib.Symbol4 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("rgba(69,69,69,0.047)").s().p("AmECpQhkAAAAhkIAAiJQAAhkBkAAIMJAAQBkAAAABkIAACJQAABkhkAAg");
+
+	this.addChild(this.shape);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(-48.9,-16.9,98,33.9);
+
+
+// stage content:
+(lib.TableContent = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_0 = function() {
+		this.Tuan1.addEventListener("click", Tuan1_ClickToGoToWebPage);
+		function Tuan1_ClickToGoToWebPage() {	
+			window.open("../index.html", "_self");	
+		}
+		this.Tuan2.addEventListener("click", Tuan2_ClickToGoToWebPage);
+		function Tuan2_ClickToGoToWebPage() {	
+			window.open("Tiet2.html", "_self");	
+		}
+		this.Tuan3.addEventListener("click", Tuan3_ClickToGoToWebPage);
+		function Tuan3_ClickToGoToWebPage() {	
+			window.open("Tiet3.html", "_self");	
+		}
+		this.Tuan4.addEventListener("click", Tuan4_ClickToGoToWebPage);
+		function Tuan4_ClickToGoToWebPage() {	
+			window.open("Tiet4.html", "_self");	
+		}
+		this.Tuan5.addEventListener("click", Tuan5_ClickToGoToWebPage);
+		function Tuan5_ClickToGoToWebPage() {	
+			window.open("Tiet5.html", "_self");	
+		}
+		this.Tuan6.addEventListener("click", Tuan6_ClickToGoToWebPage);
+		function Tuan6_ClickToGoToWebPage() {	
+			window.open("Tiet6.html", "_self");
+		}
+		this.Tuan7.addEventListener("click", Tuan7_ClickToGoToWebPage);
+		function Tuan7_ClickToGoToWebPage() {	
+			window.open("Tiet7.html", "_self");	
+		}
+		this.Tuan8.addEventListener("click", Tuan8_ClickToGoToWebPage);
+		function Tuan8_ClickToGoToWebPage() {	
+			window.open("Tiet8.html", "_self");	
+		}
+		this.Tuan9.addEventListener("click", Tuan9_ClickToGoToWebPage);
+		function Tuan9_ClickToGoToWebPage() {	
+			window.open("Tiet9.html", "_self");	
+		}
+		this.Tuan10.addEventListener("click", Tuan10_ClickToGoToWebPage);
+		function Tuan10_ClickToGoToWebPage() {	
+			window.open("Tiet 10.html", "_self");	
+		}
+		this.Tuan11.addEventListener("click", Tuan11_ClickToGoToWebPage);
+		function Tuan11_ClickToGoToWebPage() {	
+			window.open("Tiet11.html", "_self");	
+		}
+		this.Tuan12.addEventListener("click", Tuan12_ClickToGoToWebPage);
+		function Tuan12_ClickToGoToWebPage() {	
+			window.open("Tiet12.html", "_self");	
+		}
+		this.Tuan13.addEventListener("click", Tuan13_ClickToGoToWebPage);
+		function Tuan13_ClickToGoToWebPage() {	
+			window.open("Tiet13.html", "_self");	
+		}
+		this.Tuan14.addEventListener("click", Tuan14_ClickToGoToWebPage);
+		function Tuan14_ClickToGoToWebPage() {	
+			window.open("Tiet14.html", "_self");
+		}
+		this.Tuan15.addEventListener("click", Tuan15_ClickToGoToWebPage);
+		function Tuan15_ClickToGoToWebPage() {	
+			window.open("Tiet15.html", "_self");	
+		}
+		this.Tuan16.addEventListener("click", Tuan16_ClickToGoToWebPage);
+		function Tuan16_ClickToGoToWebPage() {	
+			window.open("Tiet16.html", "_self");	
+		}
+		this.Tuan17.addEventListener("click", Tuan17_ClickToGoToWebPage);
+		function Tuan17_ClickToGoToWebPage() {	
+			window.open("Tiet17.html", "_self");	
+		}
+		this.Tuan18.addEventListener("click", Tuan18_ClickToGoToWebPage);
+		function Tuan18_ClickToGoToWebPage() {	
+			window.open("Tiet18.html", "_self");	
+		}
+		this.Tuan19.addEventListener("click", Tuan19_ClickToGoToWebPage);
+		function Tuan19_ClickToGoToWebPage() {	
+			window.open("Tiet19.html", "_self");	
+		}
+		this.Tuan20.addEventListener("click", Tuan20_ClickToGoToWebPage);
+		function Tuan20_ClickToGoToWebPage() {	
+			window.open("Tiet20.html", "_self");	
+		}
+		this.Tuan21.addEventListener("click", Tuan21_ClickToGoToWebPage);
+		function Tuan21_ClickToGoToWebPage() {	
+			window.open("Tiet21.html", "_self");	
+		}
+		this.Tuan22.addEventListener("click", Tuan22_ClickToGoToWebPage);
+		function Tuan22_ClickToGoToWebPage() {	
+			window.open("Tiet22.html", "_self");	
+		}
+		this.Tuan23.addEventListener("click", Tuan23_ClickToGoToWebPage);
+		function Tuan23_ClickToGoToWebPage() {	
+			window.open("Tiet23.html", "_self");	
+		}
+		this.Tuan24.addEventListener("click", Tuan24_ClickToGoToWebPage);
+		function Tuan24_ClickToGoToWebPage() {	
+			window.open("Tiet24.html", "_self");
+		}
+		this.Tuan25.addEventListener("click", Tuan25_ClickToGoToWebPage);
+		function Tuan25_ClickToGoToWebPage() {	
+			window.open("Tiet25.html", "_self");	
+		}
+		this.Tuan26.addEventListener("click", Tuan26_ClickToGoToWebPage);
+		function Tuan26_ClickToGoToWebPage() {	
+			window.open("Tiet26.html", "_self");	
+		}
+		this.Tuan27.addEventListener("click", Tuan27_ClickToGoToWebPage);
+		function Tuan27_ClickToGoToWebPage() {	
+			window.open("Tiet27.html", "_self");	
+		}
+		this.Tuan28.addEventListener("click", Tuan28_ClickToGoToWebPage);
+		function Tuan28_ClickToGoToWebPage() {	
+			window.open("Tiet28.html", "_self");	
+		}
+		this.Tuan29.addEventListener("click", Tuan29_ClickToGoToWebPage);
+		function Tuan29_ClickToGoToWebPage() {	
+			window.open("Tiet29.html", "_self");
+		}
+		this.Tuan30.addEventListener("click", Tuan30_ClickToGoToWebPage);
+		function Tuan30_ClickToGoToWebPage() {	
+			window.open("Tiet30.html", "_self");	
+		}
+		this.Tuan31.addEventListener("click", Tuan31_ClickToGoToWebPage);
+		function Tuan31_ClickToGoToWebPage() {	
+			window.open("Tiet31.html", "_self");	
+		}
+		this.Tuan32.addEventListener("click", Tuan32_ClickToGoToWebPage);
+		function Tuan32_ClickToGoToWebPage() {	
+			window.open("Tiet32.html", "_self");	
+		}
+		this.Tuan33.addEventListener("click", Tuan33_ClickToGoToWebPage);
+		function Tuan33_ClickToGoToWebPage() {	
+			window.open("Tiet33.html", "_self");	
+		}
+		this.Tuan34.addEventListener("click", Tuan34_ClickToGoToWebPage);
+		function Tuan34_ClickToGoToWebPage() {	
+			window.open("Tiet34-35.html", "_self");	
+		}
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
+
+	// BG
+	this.Tuan1 = new lib.Symbol6();
+	this.Tuan1.setTransform(303.65,145.6);
+	new cjs.ButtonHelper(this.Tuan1, 0, 1, 1);
+	
+	this.Tuan34 = new lib.Symbol4();
+	this.Tuan34.setTransform(739.2,559.05);
+	new cjs.ButtonHelper(this.Tuan34, 0, 1, 1);
+
+	this.Tuan33 = new lib.Symbol4();
+	this.Tuan33.setTransform(739.2,558.05);
+	new cjs.ButtonHelper(this.Tuan33, 0, 1, 1);
+
+	this.Tuan32 = new lib.Symbol4();
+	this.Tuan32.setTransform(739.2,509.25);
+	new cjs.ButtonHelper(this.Tuan32, 0, 1, 1);
+	
+	this.Tuan31 = new lib.Symbol4();
+	this.Tuan31.setTransform(739.2,458.55);
+	new cjs.ButtonHelper(this.Tuan31, 0, 1, 1);
+
+	this.Tuan30 = new lib.Symbol4();
+	this.Tuan30.setTransform(739.2,406.55);
+	new cjs.ButtonHelper(this.Tuan30, 0, 1, 1);
+
+	this.Tuan29 = new lib.Symbol4();
+	this.Tuan29.setTransform(739.2,353.25);
+	new cjs.ButtonHelper(this.Tuan29, 0, 1, 1);
+
+	this.Tuan28 = new lib.Symbol4();
+	this.Tuan28.setTransform(739.2,299.7);
+	new cjs.ButtonHelper(this.Tuan28, 0, 1, 1);
+
+	this.Tuan27 = new lib.Symbol4();
+	this.Tuan27.setTransform(739.2,248.7);
+	new cjs.ButtonHelper(this.Tuan27, 0, 1, 1);
+
+	this.Tuan26 = new lib.Symbol4();
+	this.Tuan26.setTransform(739.2,196.85);
+	new cjs.ButtonHelper(this.Tuan26, 0, 1, 1);
+	
+	this.Tuan25 = new lib.Symbol4();
+	this.Tuan25.setTransform(739.2,145.85);
+	new cjs.ButtonHelper(this.Tuan25, 0, 1, 1);
+
+	this.Tuan24 = new lib.Symbol4();
+	this.Tuan24.setTransform(520.35,718.25);
+	new cjs.ButtonHelper(this.Tuan24, 0, 1, 1);
+
+	this.Tuan23 = new lib.Symbol4();
+	this.Tuan23.setTransform(520.35,663.65);
+	new cjs.ButtonHelper(this.Tuan23, 0, 1, 1);
+	
+	this.Tuan22 = new lib.Symbol4();
+	this.Tuan22.setTransform(520.35,614.25);
+	new cjs.ButtonHelper(this.Tuan22, 0, 1, 1);
+
+	this.Tuan21 = new lib.Symbol4();
+	this.Tuan21.setTransform(520.35,562.25);
+	new cjs.ButtonHelper(this.Tuan21, 0, 1, 1);
+
+	this.Tuan20 = new lib.Symbol4();
+	this.Tuan20.setTransform(520.35,509.45);
+	new cjs.ButtonHelper(this.Tuan20, 0, 1, 1);
+
+	this.Tuan19 = new lib.Symbol4();
+	this.Tuan19.setTransform(520.35,456.95);
+	new cjs.ButtonHelper(this.Tuan19, 0, 1, 1);
+
+	this.Tuan18 = new lib.Symbol4();
+	this.Tuan18.setTransform(520.35,406.8);
+	new cjs.ButtonHelper(this.Tuan18, 0, 1, 1);
+
+	this.Tuan17 = new lib.Symbol4();
+	this.Tuan17.setTransform(520.35,353.25);
+	new cjs.ButtonHelper(this.Tuan17, 0, 1, 1);
+
+	this.Tuan16 = new lib.Symbol4();
+	this.Tuan16.setTransform(520.35,299.7);
+	new cjs.ButtonHelper(this.Tuan16, 0, 1, 1);
+
+	this.Tuan15 = new lib.Symbol4();
+	this.Tuan15.setTransform(520.35,249.55);
+	new cjs.ButtonHelper(this.Tuan15, 0, 1, 1);
+
+	this.Tuan14 = new lib.Symbol4();
+	this.Tuan14.setTransform(520.35,196.85);
+	new cjs.ButtonHelper(this.Tuan14, 0, 1, 1);
+
+	this.Tuan13 = new lib.Symbol4();
+	this.Tuan13.setTransform(520.35,145.85);
+	new cjs.ButtonHelper(this.Tuan13, 0, 1, 1);
+
+	this.Tuan12 = new lib.Symbol4();
+	this.Tuan12.setTransform(304.6,718);
+	new cjs.ButtonHelper(this.Tuan12, 0, 1, 1);
+
+	this.Tuan11 = new lib.Symbol4();
+	this.Tuan11.setTransform(304.6,664.7);
+	new cjs.ButtonHelper(this.Tuan11, 0, 1, 1);
+
+	this.Tuan10 = new lib.Symbol4();
+	this.Tuan10.setTransform(304.6,612.7);
+	new cjs.ButtonHelper(this.Tuan10, 0, 1, 1);
+
+	this.Tuan9 = new lib.Symbol4();
+	this.Tuan9.setTransform(304.6,559.4);
+	new cjs.ButtonHelper(this.Tuan9, 0, 1, 1);
+
+	this.Tuan8 = new lib.Symbol4();
+	this.Tuan8.setTransform(304.6,507.4);
+	new cjs.ButtonHelper(this.Tuan8, 0, 1, 1);
+
+	this.Tuan7 = new lib.Symbol4();
+	this.Tuan7.setTransform(304.6,456.7);
+	new cjs.ButtonHelper(this.Tuan7, 0, 1, 1);
+
+	this.Tuan6 = new lib.Symbol4();
+	this.Tuan6.setTransform(304.6,404.7);
+	new cjs.ButtonHelper(this.Tuan6, 0, 1, 1);
+
+	this.Tuan5 = new lib.Symbol4();
+	this.Tuan5.setTransform(304.6,353.6);
+	new cjs.ButtonHelper(this.Tuan5, 0, 1, 1);
+
+	this.Tuan4 = new lib.Symbol4();
+	this.Tuan4.setTransform(304.6,300.8);
+	new cjs.ButtonHelper(this.Tuan4, 0, 1, 1);
+
+	this.Tuan3 = new lib.Symbol4();
+	this.Tuan3.setTransform(304.6,248.8);
+	new cjs.ButtonHelper(this.Tuan3, 0, 1, 1);
+
+	this.Tuan2 = new lib.Symbol4();
+	this.Tuan2.setTransform(304.6,197);
+	new cjs.ButtonHelper(this.Tuan2, 0, 1, 1);
+
+	this.instance = new lib.xau();
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance},{t:this.Tuan2},{t:this.Tuan3},{t:this.Tuan4},{t:this.Tuan5},{t:this.Tuan6},{t:this.Tuan7},{t:this.Tuan8},{t:this.Tuan9},{t:this.Tuan10},{t:this.Tuan11},{t:this.Tuan12},{t:this.Tuan13},{t:this.Tuan14},{t:this.Tuan15},{t:this.Tuan16},{t:this.Tuan17},{t:this.Tuan18},{t:this.Tuan19},{t:this.Tuan20},{t:this.Tuan21},{t:this.Tuan22},{t:this.Tuan23},{t:this.Tuan24},{t:this.Tuan25},{t:this.Tuan26},{t:this.Tuan27},{t:this.Tuan28},{t:this.Tuan29},{t:this.Tuan30},{t:this.Tuan31},{t:this.Tuan32},{t:this.Tuan34},{t:this.Tuan1}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(510,380,1020,760);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
